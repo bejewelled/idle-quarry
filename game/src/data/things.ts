5ce83a0f7f9e10303ffd56d26d8f779e80697a62
bejewelled <jtg7sz@virginia.edu>
2023-04-13 17:32:24 -0400

    concurrentAdders init

--- game/src/data/things.ts
+++ game/src/data/things.ts
@@ -5,28 +5,74 @@ import Decimal from "break_infinity.js";
 /**
  * @param {any} context
  * 
- * for a singular Decimal value
+ * things (L1) store
  */
-function single(context) {
+function thingManager(context) {
     // @ts-ignore
     const {subscribe, set, update, get} = writable(context);
     return {
         subscribe,
+        set(amt) {
+            update(i => {
+                if (typeof i === 'object') return amt;
+                else return new Decimal(amt);
+            })
+        },
         add(amt) {
             update(i => {
-                if (typeof i === 'Decimal') return i.plus(amt);
+                if (typeof i === 'object') return i.plus(amt);
                 else return new Decimal(i).plus(new Decimal(amt));
             })
         },
         sub(amt, negatable = false) {
             update(i => {
                 let c;
-                if (typeof i === 'Decimal') c = i.minus(amt);
+                if (typeof i === 'object') c = i.minus(amt);
                 else c = new Decimal(i).minus(new Decimal(amt));
                 if (negatable) return c;
                 else return c.max(0);
             })
-        }
+        },
+        multiply(amt) {
+            update(i => {
+                if (typeof i === 'object') return i.times(amt);
+                else return new Decimal(i).times(new Decimal(amt));
+            })
+        },
+    }
+}
+
+/**
+ * @param {any} context
+ * 
+ * for a singular Decimal value
+ */
+ function single(context) {
+    // @ts-ignore
+    const {subscribe, set, update, get} = writable(context);
+    return {
+        subscribe,
+        add(amt) {
+            update(i => {
+                if (typeof i === 'object') return i.plus(amt);
+                else return new Decimal(i).plus(new Decimal(amt));
+            })
+        },
+        sub(amt, negatable = false) {
+            update(i => {
+                let c;
+                if (typeof i === 'object') c = i.minus(amt);
+                else c = new Decimal(i).minus(new Decimal(amt));
+                if (negatable) return c;
+                else return c.max(0);
+            })
+        },
+        multiply(amt) {
+            update(i => {
+                if (typeof i === 'object') return i.times(amt);
+                else return new Decimal(i).times(new Decimal(amt));
+            })
+        },
     }
 }
 /**
@@ -43,15 +89,21 @@ function array(context) {
         addToIndex(amt, index) {
             update(i => {
                 if (index < i.length && index >= 0)
-                    if (typeof i[index] === 'Decimal') return i[index].plus(amt);
+                    if (typeof i[index] === 'object') return i[index].plus(amt);
                     else return new Decimal(i[index]).plus(new Decimal(amt));
+            })
+        },
+        set(amt) {
+            update(i => {
+                if (typeof i === 'object') return amt;
+                else return new Decimal(amt);
             })
         },
         subFromIndex(amt, index, negatable = false) {
             update(i => {
                 if (index < i.length && index >= 0) {
                     let c;
-                    if (typeof i[index] === 'Decimal') c = i[index].minus(amt);
+                    if (typeof i[index] === 'object') c = i[index].minus(amt);
                     else c = new Decimal(i[index]).minus(new Decimal(amt));
                     if (negatable) i[index] = c;
                     else  i[index] = c.max(0);
@@ -63,10 +115,17 @@ function array(context) {
 }
 
 // things (L1)
-export const things = single(new Decimal(0));
+export const things = thingManager(new Decimal(0));
 
 // thing (L1) producers
-export const thingProds = array(new Array(8).fill(new Decimal(0)));
+export const thingProds = array(new Array(8).fill(new Decimal(2)));
+// actual prod values
+export const thingProdsVal = array(Array(8).fill(new Decimal(0.1))
+.map((val, index) => index === 0 ? 1 : val)); // set index 0 == 1
+// base prod values (ideally, don't use this, just hard-code this on reset, but it's here for reference)
+export const thingProdsBase = array(Array(8).fill(new Decimal(0.1))
+.map((val, index) => index === 0 ? 1 : val)); // set index 0 == 1
+
 
 // thing (L1) multiplier from things (L1)
 export const thingsBoost = single(new Decimal(1));
